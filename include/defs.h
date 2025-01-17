588c6c639e8f50faba21d2f8c2e9f08a3915ca73
ebini <ebini@student.42lyon.fr>
2025-01-17 15:29:54 +0100

    update headers

--- include/defs.h
+++ include/defs.h
@@ -6,7 +6,7 @@
 /*   By: ebini <ebini@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 16:22:00 by ebini             #+#    #+#             */
-/*   Updated: 2025/01/07 17:13:39 by ebini            ###   ########lyon.fr   */
+/*   Updated: 2025/01/17 15:29:28 by ebini            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 # include <stdbool.h>
 # include <stddef.h>
 
+# define MEMORY_SIZE 4096;
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }		t_list;
+
 
 # define DEFS_H
 #endif
