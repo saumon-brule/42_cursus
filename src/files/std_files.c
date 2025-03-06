188a4484952cf77b7bd80820f458b4539c9b43d2
ebini <ebini@student.42lyon.fr>
2025-03-06 01:04:26 +0100

    fix norm

--- src/files/std_files.c
+++ src/files/std_files.c
@@ -6,7 +6,7 @@
 /*   By: ebini <ebini@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 19:32:46 by ebini             #+#    #+#             */
-/*   Updated: 2025/02/09 20:01:30 by ebini            ###   ########lyon.fr   */
+/*   Updated: 2025/03/06 01:02:57 by ebini            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 t_file	std_file(int fd)
 {
 	static t_pointed_file	std_files[3] = {
-		{fd: 0, flags: O_RDONLY},
-		{fd: 1, flags: O_WRONLY},
-		{fd: 2, flags: O_WRONLY}
+	{fd: 0, flags: O_RDONLY},
+	{fd: 1, flags: O_WRONLY},
+	{fd: 2, flags: O_WRONLY}
 	};
 
 	return (std_files + fd);
