1715ac97ae6b78459d71be5e12820b9670486dfa
ebini <ebini@student.42lyon.fr>
2025-03-21 15:17:57 +0100

    add macro to map AZERTY/QWERTY keys

--- include/settings.h
+++ include/settings.h
@@ -6,12 +6,14 @@
 /*   By: ebini <ebini@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 01:47:36 by ebini             #+#    #+#             */
-/*   Updated: 2025/03/19 00:37:48 by ebini            ###   ########lyon.fr   */
+/*   Updated: 2025/03/21 15:14:57 by ebini            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SETTINGS_H
 # define SETTINGS_H
+
+# include <X11/keysym.h>
 
 # define CELL_SIZE 25
 # define ZOOM 2
@@ -26,6 +28,23 @@
 # define GROUND_FRICTION 0.05
 # define WALL_FRICTION 0.01
 # define AIR_FRICTION 0.1
+
+// KEYS //
+
+# define SET_AZERTY_MAPPING
+
+# ifdef SET_AZERTY_MAPPING
+#  define GAME_KEY_LEFT XK_q
+#  define GAME_KEY_UP XK_z
+# else
+#  define GAME_KEY_LEFT XK_a
+#  define GAME_KEY_UP XK_w
+# endif
+
+# define GAME_KEY_DOWN XK_s
+# define GAME_KEY_RIGHT XK_d
+# define GAME_KEY_DEBUG XK_F3
+# define GAME_KEY_EXIT XK_Escape
 
 # define JUMP_SPEED 200
 
--- src/events/on_key_release.c
+++ src/events/on_key_release.c
@@ -6,7 +6,7 @@
 /*   By: ebini <ebini@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 04:25:35 by ebini             #+#    #+#             */
-/*   Updated: 2025/03/18 23:17:00 by ebini            ###   ########lyon.fr   */
+/*   Updated: 2025/03/21 15:16:05 by ebini            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@
 
 int	on_key_release(int keycode, t_game *game)
 {
-	if (keycode == XK_a)
+	if (keycode == GAME_KEY_LEFT)
 		game->player->acc.x += MOVEMENT_ACCELERATION;
-	else if (keycode == XK_d)
+	else if (keycode == GAME_KEY_RIGHT)
 		game->player->acc.x -= MOVEMENT_ACCELERATION;
-	else if (keycode == XK_w)
+	else if (keycode == GAME_KEY_UP)
 		game->player->acc.y += MOVEMENT_ACCELERATION;
-	else if (keycode == XK_s)
+	else if (keycode == GAME_KEY_DOWN)
 		game->player->acc.y -= MOVEMENT_ACCELERATION;
 	return (0);
 }
