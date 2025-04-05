85429425ce281be3c85ec8750ac8215e3cb42776
ebini <ebini@student.42lyon.fr>
2025-04-05 09:09:53 +0200

    add wall textures and some maps

--- include/sources.h
+++ include/sources.h
@@ -6,7 +6,7 @@
 /*   By: ebini <ebini@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 01:48:27 by ebini             #+#    #+#             */
-/*   Updated: 2025/04/04 10:40:30 by ebini            ###   ########lyon.fr   */
+/*   Updated: 2025/04/05 07:57:04 by ebini            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define SOURCES_H
 
 # define TEXTURE_COIN "./assets/textures/cake.xpm"
-# define TEXTURE_WALL "./assets/textures/wall_test.xpm"
+# define TEXTURE_WALL "./assets/textures/wall.xpm"
 # define TEXTURE_AIR "./assets/textures/air.xpm"
 # define TEXTURE_EXIT "./assets/textures/exit.xpm"
 # define TEXTURE_PLAYER "./assets/textures/cube.xpm"
